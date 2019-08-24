be7cfda66f85d31e3fe057cf2214759b1b88f3f5
gsandhu916 <gsandhu916@gmail.com>
2019-08-23 20:55:36 -0700

    landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const bars = <FontAwesomeIcon className='icon' icon={faBars} />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='front-page-container'>
+      {/* <img className='img' src='https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_050.jpg?mode=max&quality=90&scale=down' alt='car' /> */}
+      <div className='sidebar'>
+        <div className='top-side'>
+        <p>{bars} McLaren Speedtail</p>
+    </div>
+      </div>
+      <div className='content-container'>
+        <input placeholder='Search by model' />
+        <div className='car-title'>
+          <h1 className='h1-color'>Heir To the Throne</h1>
+          <h1>McLaren Speedtail</h1>
+          <h1>Unveiled</h1>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
