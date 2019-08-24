213143ae8c0453ac5cc05ccdf836145f366e5014
gsandhu916 <gsandhu916@gmail.com>
2019-08-23 23:29:31 -0700

    added photo slide with open window and close window

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
+
+import PhotoCarousel from './PhotoCarousel';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faImages } from '@fortawesome/free-solid-svg-icons'
 
 const bars = <FontAwesomeIcon className='icon' icon={faBars} />
+const imgs = <FontAwesomeIcon className='icon' icon={faImages} />
 
 function App() {
+
+  let [showPics, setShowPics] = useState(false)
+
+  let styles= {
+    background: {
+      backgroundImage: 'url("https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_050.jpg?mode=max&quality=90&scale=down")'
+    },
+    showPhotos: {
+      opacity: 1,
+      visibility: 'visible'
+    },
+    hidePhotos: {
+      opacity: 0,
+      visibility: 'hidden'
+    }
+  }
+
+  const pages = [
+    'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_051.jpg?mode=max&quality=90&scale=down',
+    'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_052.jpg?mode=max&quality=90&scale=down',
+    'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_053.jpg?mode=max&quality=90&scale=down',
+    'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_054.jpg?mode=max&quality=90&scale=down',
+    'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_056.jpg?mode=max&quality=90&scale=down'
+  ]
+
+  const clickHandle = () => {
+    setShowPics(!showPics)
+    console.log('clicked')
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={styles.background}>
       <div className='front-page-container'>
       {/* <img className='img' src='https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_050.jpg?mode=max&quality=90&scale=down' alt='car' /> */}
       <div className='sidebar'>
         <div className='top-side'>
+        <div style={showPics ? styles.showPhotos : styles.hidePhotos} className='photos'>
+        <PhotoCarousel clickHandle={clickHandle} pages={pages} />
+      </div>
         <p>{bars} McLaren Speedtail</p>
     </div>
       </div>
       <div className='content-container'>
         <input placeholder='Search by model' />
         <div className='car-title'>
-          <h1 className='h1-color'>Heir To the Throne</h1>
-          <h1>McLaren Speedtail</h1>
+          <h1 className='h1-thin'>Heir To the Throne</h1>
+          <h1 className='h1-bold'>McLaren Speedtail</h1>
           <h1>Unveiled</h1>
         </div>
+        <div onClick={clickHandle} className='btn-container'>
+      <div className='img-btn'>{imgs}</div>
+      <p>View Photos</p>
+      </div>
       </div>
       </div>
     </div>
---
+++ src/PhotoCarousel.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react'
+import clamp from 'lodash-es/clamp'
+import { useSprings, animated } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+const close = <FontAwesomeIcon size='3x' className='icon' icon={faWindowClose} />
+
+// const pages = [
+//   'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_051.jpg?mode=max&quality=90&scale=down',
+//   'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_052.jpg?mode=max&quality=90&scale=down',
+//   'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_053.jpg?mode=max&quality=90&scale=down',
+//   'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_054.jpg?mode=max&quality=90&scale=down',
+//   'https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_056.jpg?mode=max&quality=90&scale=down'
+// ]
+
+const PhotoCarousel = ({pages, clickHandle}) => {
+  const index = useRef(0)
+  const [props, set] = useSprings(pages.length, i => ({ x: i * window.innerWidth, sc: 1, display: 'block' }))
+  const bind = useGesture(({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
+    if (down && distance > window.innerWidth / 2)
+      cancel((index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, pages.length - 1)))
+    set(i => {
+      if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
+      const x = (i - index.current) * window.innerWidth + (down ? xDelta : 0)
+      const sc = down ? 1 - distance / window.innerWidth / 2 : 1
+      return { x, sc, display: 'block' }
+    })
+  })
+  return props.map(({ x, display, sc }, i) => (
+    <animated.div {...bind()} key={i} style={{ display, transform: x.interpolate(x => `translate3d(${x}px,0,0)`) }}>
+    <span onClick={clickHandle} className='x-span'>{close}</span>
+      <animated.div style={{ transform: sc.interpolate(s => `scale(${s})`), backgroundImage: `url(${pages[i]})` }} />
+    </animated.div>
+  ))
+}
+
+export default PhotoCarousel;
