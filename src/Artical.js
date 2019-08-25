d41700aec209ec3cbf5821cf349891dee2615955
gsandhu916 <gsandhu916@gmail.com>
2019-08-24 22:35:19 -0700

    desktop friendly need to work on mobile

---
+++ src/Artical.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Artical = () => {
+  return(
+    <div className='press-container'>
+    <div className='overview'>
+    <div className='press-img-container'>
+      <img src='https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_050.jpg?mode=max&quality=90&scale=down' alt='car' />
+      <img src='https://car-images.bauersecure.com/pagefiles/68068/1752x1168/speedtail_051.jpg?mode=max&quality=90&scale=down' alt='car' />
+      </div>
+      <div className='artical'>
+      <h3>Overview</h3>
+      <p>
+      <span style={{color: '#A68E90', fontWeight: 'bold', fontSize: 23}}>A</span>s the spiritual successor to the legendary McLaren F1, the 2020 McLaren Speedtail evokes similar desires with its one-of-a-kind design and out-of-this-world performance. The streamlined speedster is the fastest and most powerful McLaren ever built, and it represents the pinnacle of the company's exotic lineup.
+      </p>
+      <h3>Engine Ride and Handling</h3>
+      <p>
+      <span style={{color: '#A68E90', fontWeight: 'bold', fontSize: 23}}>W</span>hile full powertrain details are unknown, we do know that the rear-drive 2020 Speedtail has a hybrid system that develops a total of 1035 horsepower courtesy of its gasoline engine and electric motors. Combined with an incredibly aerodynamic shape and ultra-lightweight construction, the Speedtail can reach a claimed top speed of 250 mph. Likewise, McLaren says it will accelerate from zero to 186 mph in 12.8 seconds. That makes it significantly quicker than the almighty McLaren P1, which did the deed in 16.5 seconds. It has a set of Pirelli P Zero tires that were specially developed to handle extreme speeds and wrap the staggered 20- and 21-inch wheels. The hybrid hypercar also can be lowered by 1.4 inches and includes a Velocity drive mode that optimizes aerodynamics and powertrain settings for high-speed runs.
+      </p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Artical;
